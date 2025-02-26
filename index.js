6a01b6e75c966df1aaa34f0c21d685b6cf51ebd6
jiel <nikhilpagadala2006@gmail.com>
2025-02-26 09:58:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express =require('express')
+const app =express()
+port =3000
+const users =[
+    {username:"alice",age:25,email:"alice@gmail.com"},
+    {username:"jhon",age:25,email:"jhon@gmail.com"},
+    {username:"nikhil",age:25,email:"jhon@gmail.com"}
+]
+app.use(express.json())
+app.get('/users',(req,res)=>{
+    if(!req.query.user){
+          return res.json({"message":"Usercannot be empty"})
+    }
+    const user =users.find(u=>{u.username===req.query.user})
+    if(user){
+        return  res.json({"message":"User found","data":user})
+    }
+    else{
+        return  res.json({"message":"User not found"})
+     }
+    
+})
+
+   
+
+app.listen(port,()=>{
+    console.log(`server is running on port:${port}`)
+})
---
+++ readme.md
@@ -0,0 +1,3 @@
+github repository link : https://github.com/Nikhil2510192/BACKEND-CA-2
+
+deployment link:
